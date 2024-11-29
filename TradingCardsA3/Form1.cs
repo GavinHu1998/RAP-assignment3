1e2eb7fd75f33ea04ca507d0b1e9f84302fc32c6
Gavin <156557247+GavinHu1998@users.noreply.github.com>
2024-11-29 12:43:29 -0700

    Update Form1.cs change
    Gavin finished the buttonAddCard_Click function

--- TradingCardsA3/Form1.cs
+++ TradingCardsA3/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,8 +20,10 @@ namespace TradingCardsA3
             InitializeComponent();
             InitializePlayers();
 
-            
-        }
+            //private Dictionary<string, Image> playerPhotos = new Dictionary<string, Image>();
+
+
+    }
     
 
         public class Player
@@ -108,8 +110,113 @@ namespace TradingCardsA3
 
         private void buttonAddCard_Click(object sender, EventArgs e)
         {
-            Player newPlayer = new Player("Cristiano Ronaldo", "Juventus Legends", Properties.Resources.CristianoRonaldo, "Forward", 12, 4, 18, 2);
-            bindingSource1.Add(newPlayer);
+            // Create a form to input new player details
+            Form addPlayerForm = new Form();
+            addPlayerForm.Text = "Add New Player";
+            addPlayerForm.Size = new Size(300, 500);
+
+            // Create input fields
+            Label[] labels = new Label[]
+            {
+        new Label { Text = "Name:", Location = new Point(10, 20) },
+        new Label { Text = "Team:", Location = new Point(10, 60) },
+        new Label { Text = "Position:", Location = new Point(10, 100) },
+        new Label { Text = "Goals:", Location = new Point(10, 140) },
+        new Label { Text = "Assists:", Location = new Point(10, 180) },
+        new Label { Text = "Matches Played:", Location = new Point(10, 220) },
+        new Label { Text = "Yellow Cards:", Location = new Point(10, 260) }
+            };
+
+            TextBox[] textBoxes = new TextBox[]
+            {
+        new TextBox { Location = new Point(150, 20), Width = 120 },
+        new TextBox { Location = new Point(150, 60), Width = 120 },
+        new TextBox { Location = new Point(150, 100), Width = 120 },
+        new TextBox { Location = new Point(150, 140), Width = 120 },
+        new TextBox { Location = new Point(150, 180), Width = 120 },
+        new TextBox { Location = new Point(150, 220), Width = 120 },
+        new TextBox { Location = new Point(150, 260), Width = 120 }
+            };
+
+            Button btnSelectPhoto = new Button
+            {
+                Text = "Select Photo",
+                Location = new Point(10, 300),
+                Width = 120
+            };
+
+            Button btnSave = new Button
+            {
+                Text = "Save Player",
+                Location = new Point(150, 300),
+                Width = 120
+            };
+
+            Image selectedPhoto = null;
+
+            // Photo selection event
+            btnSelectPhoto.Click += (s, ev) =>
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        selectedPhoto = Image.FromFile(openFileDialog.FileName);
+                    }
+                }
+            };
+
+            // Save player event
+            btnSave.Click += (s, ev) =>
+            {
+                try
+                {
+                    // Validate inputs
+                    if (string.IsNullOrWhiteSpace(textBoxes[0].Text) ||
+                        string.IsNullOrWhiteSpace(textBoxes[1].Text) ||
+                        selectedPhoto == null)
+                    {
+                        MessageBox.Show("Please fill in all required fields and select a photo.");
+                        return;
+                    }
+
+                    // Create new player
+                    Player newPlayer = new Player(
+                        textBoxes[0].Text,           // Name
+                        textBoxes[1].Text,            // Team
+                        selectedPhoto,                // Photo
+                        textBoxes[2].Text,            // Position
+                        int.Parse(textBoxes[3].Text), // Goals
+                        int.Parse(textBoxes[4].Text), // Assists
+                        int.Parse(textBoxes[5].Text), // Matches Played
+                        int.Parse(textBoxes[6].Text)  // Yellow Cards
+                    );
+
+                    // Add player to the list
+                    bindingSource1.Add(newPlayer);
+
+                    // Close the form
+                    addPlayerForm.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error adding player: " + ex.Message);
+                }
+            };
+
+            // Add controls to the form
+            for (int i = 0; i < labels.Length; i++)
+            {
+                addPlayerForm.Controls.Add(labels[i]);
+                addPlayerForm.Controls.Add(textBoxes[i]);
+            }
+            addPlayerForm.Controls.Add(btnSelectPhoto);
+            addPlayerForm.Controls.Add(btnSave);
+
+            // Show the form
+            addPlayerForm.ShowDialog();
         }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
